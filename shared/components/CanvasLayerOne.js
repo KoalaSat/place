183c7e3fb2d54850b7c5ccd83f36ca335265de8a
colorlessenergy <brianmunozdev@gmail.com>
2022-04-15 10:42:37 -0700

    move select pixel icon outside handle canvas click function

--- shared/components/CanvasLayerOne.js
+++ shared/components/CanvasLayerOne.js
@@ -8,21 +8,20 @@ const CanvasLayerOne = ({ setSelectedPixel }) => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
+        const selectPixelIcon = new Image(10, 10);
+        selectPixelIcon.src = '/select-icon.png';
         const handleCanvasClick = event => {
             context.clearRect(0, 0, canvas.width, canvas.height);
 
-            const selectPixelIcon = new Image(10, 10);
-            selectPixelIcon.src = '/select-icon.png';
             const pixelX = Math.floor(event.offsetX / PIXEL_SIZE) * PIXEL_SIZE;
             const pixelY = Math.floor(event.offsetY / PIXEL_SIZE) * PIXEL_SIZE;
-            selectPixelIcon.onload = () => {
-                context.drawImage(selectPixelIcon, pixelX, pixelY);
-            };
 
             setSelectedPixel({
                 x: pixelX,
                 y: pixelY
             });
+
+            context.drawImage(selectPixelIcon, pixelX, pixelY);
         };
 
         canvas.addEventListener('click', handleCanvasClick);
